203203bda59ba97aa251fd4155dda080a13fec0d
galeheidi <kgaletax@aol.com>
2018-06-20 19:14:09 +0000

    automatically backed up by learn

--- file.rb
+++ file.rb
@@ -1,7 +1,7 @@
 ingredients = [
-"bread"=["rye", "sourdough", "baguette"],
-"meat" =["ham", "tofu", "turkey"],
-"cheese"=["swiss", "munster", "cheddar"],
-"sauce"=["mayo", "mustard", "tabasco"]
+"bread"= ["rye", "sourdough", "baguette"],
+"meat" = ["ham", "tofu", "turkey"],
+"cheese"= ["swiss", "munster", "cheddar"],
+"sauce"= ["mayo", "mustard", "tabasco"]
 ]
 puts ingredients [0][0][1][1][2][2]
